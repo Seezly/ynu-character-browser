72b77e4934d887da0ecbe8949b30ac86468b3929
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-03-19 15:39:06 -0400

    feat: Unnecesary files deleted

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello ReactJS!</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
