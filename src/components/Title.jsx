410c18027070ea850fca6f179c2c1dbf2d296df8
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-03-19 17:55:42 -0400

    feat: Title.jsx component added

---
+++ src/components/Title.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Title extends React.Component {
+    render() {
+        return (
+            <div
+                className="title">
+                <h1>The Song of the Night Walkers</h1>
+                <h2>よふかしのうた</h2>
+            </div>
+        )
+    }
+}
+
+export default Title;
