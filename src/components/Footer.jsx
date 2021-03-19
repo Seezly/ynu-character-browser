97cbcbf3e403cb0ae606599b66b2738d2734bda6
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-03-19 18:05:30 -0400

    feat: Footer.jsx component added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                <h6>designed & developed by <a href="https://www.sergiogutierrez.ml" target="_blank" rel="noopener noreferrer">Seezly</a>.</h6>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
