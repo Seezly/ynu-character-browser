63b6b96ad921e090c8e83b7637fbed298d65d13e
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-03-19 18:01:35 -0400

    feat: Header.jsx component added

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Title from './Title'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <Title />
+            </header>
+        )
+    }
+}
+
+export default Header;
