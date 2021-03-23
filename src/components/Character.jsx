9508dcff1b58a875ee9be1f7b74c4a725f964f3a
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-03-23 13:03:21 -0400

    feat: Character.jsx added

---
+++ src/components/Character.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Character extends React.Component {
+    render() {
+        return (
+            <div
+                className="character">
+                <div 
+                    className="character__img" 
+                    data-url={this.props.img} 
+                    style={
+                        {
+                            background: `url(${'.' + this.props.img}) center no-repeat`,
+                            backgroundSize: 'cover'
+                        }
+                    } />
+                <h3>{this.props.name}</h3>
+                <h4>{this.props.race}</h4>
+                <p>Age: {this.props.age}</p>
+                <p>Gender: {this.props.gender}</p>
+                <p>Status: {this.props.status}</p>
+            </div>
+        )
+    }
+};
+
+export default Character;
